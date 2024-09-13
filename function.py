316bb49be9b1d6851679b619c676a66b3fc9ea16
PhilipBugahod <philip.bugahod@dawsoncollege.qc.ca>
2024-09-13 23:20:22 +0000

    activity 2

---
+++ function.py
@@ -0,0 +1,7 @@
+
+def calculatearea(radius) : 
+    area = 3.14 * radius ** 2 
+    return area 
+
+circle1 = calculatearea(3) 
+print(circle1)
---
+++ tax.py
@@ -0,0 +1,6 @@
+price = float(input("Enter the price of the food: "))
+pricetaxincluded = price * 1.18
+print("This is the price tax included", int(pricetaxincluded))
+
+x = "*" *3
+print(x)
